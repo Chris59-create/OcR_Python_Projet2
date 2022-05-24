e70587f693c446d535c37698a9cd81ef3c92a149
Chris Create <chris59create@gmail.com>
2022-05-24 16:32:44 +0200

    + extraction titre et description

--- extraction_prix.py
+++ extraction_prix.py
@@ -8,27 +8,30 @@ recherche = ["universal_ product_code (upc)", "title", "price_including_tax",
 
 print(recherche)
 
-# Script extraction des informations  de la page produit
+# Extraction à partir page produit
+# Récupération code source de la page
 product_page_url = ("http://books.toscrape.com/catalogue/a-light-in-the"
                     "-attic_1000/index.html")
 page = requests.get(product_page_url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
+# Extraction des données dispersées
+extracts = {}
 title = soup.find("li", class_="active")
-print(title.string)
-
+extracts["title"] = title.string
+p = soup.findAll('p', {})
+extracts["product_description"] = p[3].string
+category = soup.find_all('a',
+                         href='http://books.toscrape.com/catalogue/category/books/poetry_23/index.html')
+print("category", category)
 #extraction données du tableau html
 trs = soup.find_all("tr")
-print(trs)
-extracts = {}
 for tr in trs:
     th = tr.find("th")
     td = tr.find("td")
     extracts[th.string] = td.string
 print(extracts)
 
-title = soup.find("li", class_="active")
-print(title.string)
 # Dictionnaire de correspondence
 correspondence = {
     "universal_ product_code (upc)": "UPC",
