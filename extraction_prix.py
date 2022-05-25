ecad2f936a397445447b0f740713e773bfee122d
Chris Create <chris59create@gmail.com>
2022-05-25 10:40:38 +0200

    création bookstocrape.csv avec en_tete

--- extraction_prix.py
+++ extraction_prix.py
@@ -1,15 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
-recherche = ["universal_ product_code (upc)", "title", "price_including_tax",
-    "price_excluding_tax", "number_available", "product_description",
-    "category", "review_rating", "image_url"
-    ]
-
-print(recherche)
+import csv
 
 # Extraction à partir page produit
+
 # Récupération code source de la page
 product_page_url = ("http://books.toscrape.com/catalogue/a-light-in-the"
                     "-attic_1000/index.html")
@@ -36,7 +31,6 @@ img_tag = image_tag.img
 image_url = "htpp://books.toscrape.com" + img_tag['src'][5:]
 extracts["image_url"] = image_url
 
-
 # Extraction données du tableau html et ajout dans dictionnaire
 trs = soup.find_all("tr")
 for tr in trs:
@@ -46,6 +40,13 @@ for tr in trs:
 print(extracts)
 
 # Ecriture des données dans fichier csv
+en_tete = ["universal_ product_code (upc)", "title", "price_including_tax",
+    "price_excluding_tax", "number_available", "product_description",
+    "category", "review_rating", "image_url"
+    ]
+with open('bookstoscrape_data.csv', 'w') as file_csv:
+    writer = csv.writer(file_csv, delimiter=',')
+    writer.writerow(en_tete)
 
 # Dictionnaire de correspondence
 correspondence = {
