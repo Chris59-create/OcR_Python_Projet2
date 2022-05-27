16cf25632008eca0c3cc45de28637ca18b7d99c2
Chris Create <chris59create@gmail.com>
2022-05-27 05:51:03 +0200

    Remise dans le bon ordre des fonctions dans extraction_prix

--- extraction_prix.py
+++ extraction_prix.py
@@ -5,24 +5,27 @@ import csv
 
 # Récupération des liens des pages produits pour la catégorie
 
-# Ecriture des données dans fichier csv
-def csvEcrit(extracts):
-    en_tete = ["universal_ product_code (upc)", "title", "price_including_tax",
-        "price_excluding_tax", "number_available", "product_description",
-        "category", "review_rating", "image_url"
-        ]
-    source_en_tete = ["UPC", "title", "Price (incl. tax)", "Price (excl. tax)",
-        "Availability", "product_description", "category", "star-rating",
-        "image_url"
-        ]
-    source_data = []
-    for destination_key, source_key in zip(en_tete, source_en_tete):
-        item = extracts[source_key]
-        source_data.append(item)
-    with open('bookstoscrape_data.csv', 'a') as file_csv:
-        writer = csv.writer(file_csv, delimiter=',')
-        writer.writerow(en_tete)
-        writer.writerow(source_data)
+
+# Récupération code source de la page produit
+def productPageContent(url):
+    product_page_url = url
+    page = requests.get(product_page_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    print("Soup : ", soup)
+    return soup
+
+
+# Liste de toutes les url produit de la page category
+def urlBooksExtract(category_url):
+    urlBooksList = []
+    #page_category_url = category_url
+    page_category = requests.get(category_url)
+    soup_category = BeautifulSoup(page_category.content, 'html.parser')
+    for href in soup_category.find_all('a', title=True):
+        urlBook = "http://books.toscrape.com/catalogue" + href['href'][8:]
+        urlBooksList.append(urlBook)
+    print("liste url books", urlBooksList)
+    return urlBooksList
 
 
 # Création du dictionnaire des données du produit
@@ -59,26 +62,25 @@ def dataDict(soup):
     return extracts
 
 
-# Récupération code source de la page produit
-def productPageContent(url):
-    product_page_url = url
-    page = requests.get(product_page_url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    print("Soup : ", soup)
-    return soup
+# Ecriture des données dans fichier csv
+def csvEcrit(extracts):
+    en_tete = ["universal_ product_code (upc)", "title", "price_including_tax",
+        "price_excluding_tax", "number_available", "product_description",
+        "category", "review_rating", "image_url"
+        ]
+    source_en_tete = ["UPC", "title", "Price (incl. tax)", "Price (excl. tax)",
+        "Availability", "product_description", "category", "star-rating",
+        "image_url"
+        ]
+    source_data = []
+    for destination_key, source_key in zip(en_tete, source_en_tete):
+        item = extracts[source_key]
+        source_data.append(item)
+    with open('bookstoscrape_data.csv', 'a') as file_csv:
+        writer = csv.writer(file_csv, delimiter=',')
+        writer.writerow(en_tete)
+        writer.writerow(source_data)
 
-
-# Liste de toutes les url produit de la page category
-def urlBooksExtract(category_url):
-    urlBooksList = []
-    #page_category_url = category_url
-    page_category = requests.get(category_url)
-    soup_category = BeautifulSoup(page_category.content, 'html.parser')
-    for href in soup_category.find_all('a', title=True):
-        urlBook = "http://books.toscrape.com/catalogue" + href['href'][8:]
-        urlBooksList.append(urlBook)
-    print("liste url books", urlBooksList)
-    return urlBooksList
 
 # Lancement de la fonction d'extraction des url produits à partir category
 urlBooksList = urlBooksExtract('http://books.toscrape.com/catalogue/category'
