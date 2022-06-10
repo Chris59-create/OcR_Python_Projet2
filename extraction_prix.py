2cfc09ee56c86bad221927930c6d99969c806f21
Chris Create <chris59create@gmail.com>
2022-06-11 00:41:11 +0200

    Dynamic messages to allow to follow the processing of the program are added in the code

--- extraction_prix.py
+++ extraction_prix.py
@@ -4,6 +4,8 @@ import re
 import os
 import csv
 
+print('\n', "Welcome! You begin to scrape http://books.toscrape.com/", '\n')
+
 # horme url of the site to scrape
 homeUrl = 'http://books.toscrape.com/'
 
@@ -11,67 +13,79 @@ homeUrl = 'http://books.toscrape.com/'
 # To store the csv files per category
 dirCsv = 'csv_files'
 if not os.path.exists(dirCsv):
+    print("The directory 'csv_files' is created in the current directory. For "
+          "each category of books, each data extracted per book is stored in "
+          "a csv file named with the name of the category. These csv_files is "
+          "located in the created directory.", '\n')
     os.mkdir(dirCsv)
+else:
+    print("The directory 'csv_files' already exists in the current directory",
+          '\n')
 # Create the directory books_img in the current directory
 # To store the subdirectories per category for books img
 dirImg = "img_directories"
 if not os.path.exists(dirImg):
+    print("The directory 'img_directories' is created in the current "
+          "directory. In this just created directroy, each image of a book is "
+          "stored in a named nameCategory_img subdirectory. The image file "
+          "is named with the UPC of the relative book", '\n')
     os.mkdir(dirImg)
+else:
+    print("The directory 'img_directories' already exists in the current "
+          "directory", '\n')
 
 # Library of functions
 
 
 # Load the source code of the page
 def productPageContent(url):
-    print('\n', "début productPageContent(url)", '\n',)# for test
     product_page_url = url
     page = requests.get(product_page_url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    #print("Soup : ", soup)
-    print('\n', "end productPageContent(url)", '\n',)# for test
     return soup
 
 
 # List of all categories's urls of the home page
 def urlCategoriesExtract(soup):
-    print('\n', "début urlCategoriesExtract(soup)", '\n')# for test
     urlCategoriesList = []
     for href in soup.find_all('a', href=re.compile("/category/")):
         urlCategory = "http://books.toscrape.com/" + href['href']
         urlCategoriesList.append(urlCategory)
     del urlCategoriesList[0]
-    print(urlCategoriesList)# for test
-    print('\n', "fin urlCategoriesExtract(soup)", '\n')# for test
+    print(f"{len(urlCategoriesList)} urls of categories are extracted", '\n')
     return urlCategoriesList
 
 # List names of categories extracted from categories urls
 def namesCategories(urlCategoriesList):
-    print('\n', "début namesCategories", '\n')# for test
     nameCategoryList = []
     for item in urlCategoriesList:
         urlCategorySplit = item.split('/')
         nameCategory = urlCategorySplit[-2].split("_")[-2]
         nameCategoryList.append(nameCategory)
-    print('\n', "fin namesCategories", '\n')# for test
+    # Preparation of print of names of category like table
+    print("-" * 72, '\n', "Names of the categories:", '\n')
+    board = []
+    for name in nameCategoryList:
+        board.append(name)
+        l = len(board)
+        if l % 5 == 0:
+            print(board[l - 5:l], '\n')
     return nameCategoryList
 
 
 # List of all book's urls of the category page
 def urlBooksExtract(category_url):
-    print('\n', "début urlBooksExtract", '\n')# for test
     urlBooksList = []
     page_category = requests.get(category_url)
     soup_category = BeautifulSoup(page_category.content, 'html.parser')
     for href in soup_category.find_all('a', title=True):
         urlBook = "http://books.toscrape.com/catalogue" + href['href'][8:]
         urlBooksList.append(urlBook)
-    print('\n', "Fin urBooksExtract", '\n')# for test
     return urlBooksList
 
 
 # Create dictionary of book data
 def dataDict(soup, urlbook):
-    print('\n', "début dataDict(soup)", '\n')# for test
     # Initialization data dictionary
     extracts = {}
 
@@ -113,15 +127,11 @@ def dataDict(soup, urlbook):
     image_url = "http://books.toscrape.com" + img_tag['src'][5:]
     extracts["image_url"] = image_url
 
-    print("extracts : ", extracts)  # for test
-    print('\n', "Fin dataDict")  # for test
-
     return extracts
 
 
 # Writing of the book data in the csv file
 def csvEcrit(extracts):
-    print('\n', "csvEcrit(extracts)", '\n')# for test
     source_data = []
     for key in extracts:
         item = extracts[key]
@@ -129,24 +139,18 @@ def csvEcrit(extracts):
     writer = csv.writer(file_csv, delimiter=',')
     writer.writerow(source_data)
 
-    print('\n', "Fin csvEcrit", '\n')# for test
-
-
 # Main code
 
 # Create the list of categories and categories names of the site
 soup = productPageContent(homeUrl)
 urlCategoriesList = urlCategoriesExtract(soup)
 namesCategoriesList= namesCategories(urlCategoriesList)
-print("namesCategoriesList : ", namesCategoriesList)# for test
 
-#
+# Treatment of each category
 for urlCategory, nameCategory in zip(urlCategoriesList, namesCategoriesList):
     # Extract root of the category url
     urlRoot = urlCategory[:-10]
-    print(urlRoot)#for test
     # Extract the code source of the first of category
-    print("urlCategory", urlCategory)#for test
     soupCategory = productPageContent(urlCategory)
     # list urls of products pages from the first page
     # of the category
@@ -155,10 +159,11 @@ for urlCategory, nameCategory in zip(urlCategoriesList, namesCategoriesList):
     while soupCategory.find('li', class_="next"):
         nextPage = soupCategory.find('a', text="next")
         nextPageUrl = urlRoot + nextPage['href']
-        print(nextPageUrl)#for test
         soupCategory = productPageContent(nextPageUrl)
         urlBooksList += urlBooksExtract(nextPageUrl)
-    print("list of book urls of category", urlBooksList)#for test
+    print("-"*72, '\n')
+    print(f"{len(urlBooksList)} url(s) of book(s) of the categorie"
+                f" {nameCategory} are extracted", '\n')
 
     # Create the csv file of the category with first row en_tete"
     # in the folder csv_files
@@ -172,8 +177,10 @@ for urlCategory, nameCategory in zip(urlCategoriesList, namesCategoriesList):
     with open(nameCsv, 'w') as file_csv:
         writer = csv.writer(file_csv, delimiter=',')
         writer.writerow(en_tete)
+        print(f"The file '{nameCsv}' is created", '\n')
 
         #Extract books data of the category
+        imgNumber = 0
         for urlBook in urlBooksList:
             soup = productPageContent(urlBook)
             extracts = dataDict(soup, urlBook)
@@ -183,6 +190,8 @@ for urlCategory, nameCategory in zip(urlCategoriesList, namesCategoriesList):
             pathCategoryImg = dirImg + "/" + nameCategory + "_img"
             if not os.path.exists(pathCategoryImg):
                 os.mkdir(pathCategoryImg)
+                print(f"The subdirectory '{pathCategoryImg}' is "
+                            f"created")
             # Download the image of the current book in the directory
             urlImg = extracts["image_url"]
             fileTypImg = urlImg.split(".")[-1]
@@ -190,7 +199,14 @@ for urlCategory, nameCategory in zip(urlCategoriesList, namesCategoriesList):
             fileName = (pathCategoryImg + "/"
                         + extracts["universal_ product_code (upc)"]
                         + "." + fileTypImg)
-            print(fileName)
             r = requests.get(urlImg)
             with open(fileName, 'wb') as file:
                 file.write(r.content)
+                imgNumber += 1
+        print(f"{imgNumber} images are downloaded for the "
+                    f"category '{nameCategory}'.", '\n')
+
+print(f"The scrapping of the url '{homeUrl}' is ended", '\n', "You can find "
+        "the extracted data in the subdiretory 'csv_files' in the current "
+        "directory and the images of the books in the subdirectory "
+        "'img_directories'.")
